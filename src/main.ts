c8156db02ec04cf73156f47260d75ff8863163b6
kkk <18782072944@163.com>
2026-05-28 18:07:53 +0800

    feat：添加vconsole

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import VConsole from 'vconsole'
 import App from './App.vue'
 import router from './router'
+
+new VConsole()
 
 if (!(Map.prototype as any).getOrInsertComputed) {
   (Map.prototype as any).getOrInsertComputed = function (key: any, callbackInsert: (key: any) => any) {
